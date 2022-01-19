055ae9a76af1f7bdb5b73fd87c2b07d5aff6bb94
Manu <manufuentes28@gmail.com>
2022-01-19 03:15:48 -0300

    test api

--- Python/main.py
+++ Python/main.py
@@ -23,16 +23,24 @@ import pandas as pd
 import json
 import logging
 
-URL = 'https://api.mercadolibre.com/sites/MLA/search?q=chromecast&limit=50#json'
+URL = 'https://api.mercadolibre.com/sites/MLA/search'
+# KEY = '' - API KEY????
+ARGS = {'q':'chromecast'}
 
-logging.basicConfig(filename='./loggers.log', format='%(levelname)s - %(asctime)s - %(message)s', level=logging.INFO)
+# logging.basicConfig(filename='./loggers.log', format='%(levelname)s - %(asctime)s - %(message)s', level=logging.INFO)
 
 if __name__ == '__main__':
-    response = r.get(URL)
+    
+    response = r.get(URL, params=ARGS)
+    
+    print(response.url)
     
     if response.status_code == 200:
-        content = response.content
+        #response_json = response.json()
+        
+        response_json = json.loads(response.text)
+        print(response_json)
         
         file = open('rocking.json', 'wb')
-        file.write(content)
+        file.write(response_json)
         file.close()
