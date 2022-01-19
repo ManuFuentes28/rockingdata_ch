697431cc7075fc06fd9538937dd19b1ccf082341
Manu <manufuentes28@gmail.com>
2022-01-19 02:33:45 -0300

    add poetry, logger

--- Python/main.py
+++ Python/main.py
@@ -17,3 +17,22 @@ Escribir los resultados en un archivo plano delimitado por comas, desnormalizand
 el JSON obtenido en el paso anterior, en tantos campos como sea necesario para
 guardar las variables que te interesen modelar.
 """
+
+import requests as r
+import pandas as pd
+import json
+import logging
+
+URL = 'https://api.mercadolibre.com/sites/MLA/search?q=chromecast&limit=50#json'
+
+logging.basicConfig(filename='./loggers.log', format='%(levelname)s - %(asctime)s - %(message)s', level=logging.INFO)
+
+if __name__ == '__main__':
+    response = r.get(URL)
+    
+    if response.status_code == 200:
+        content = response.content
+        
+        file = open('rocking.json', 'wb')
+        file.write(content)
+        file.close()
